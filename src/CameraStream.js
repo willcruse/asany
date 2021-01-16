8e9daafcfc49c1fefd49d8635857f1b82e725973
Will Cruse <willcruse123@protonmail.com>
2021-01-16 12:02:41 +0000

    Solve conflict

--- src/CameraStream.js
+++ src/CameraStream.js
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {
   useRef,
-  useEffect
+  useEffect,
+  useState,
 } from "react";
 
 import {
   Container
 } from "shards-react";
 
-function CameraStream({videoWidth, videoHeight}) {
+import * as posenet from '@tensorflow-models/posenet';
+import '@tensorflow/tfjs-backend-webgl';
+
+function CameraStream(props) {
+
+  const {videoWidth, videoHeight, modelName} = props;
 
   const videoComponent = useRef(null);
+  const canvasComponent = useRef(null);
+
+  const [model, updateModel] = useState(null);
+  const [ready, setReady] = useState(false);
 
   useEffect(() => { // Called when videoComponent is changed
     if (videoComponent.current != null) {
@@ -37,12 +47,13 @@ function CameraStream({videoWidth, videoHeight}) {
         video.srcObject = stream;
 
         const videoPlayer = new Promise(resolve => {
-          video.onloadedmetadata = () => {
+          video.onloadeddata = () => {
             video.play()
             resolve(video)
           }
         });
-        videoPlayer.then(() => {})
+        videoPlayer.then(() => {});
+        setReady(true)
       } catch (error) {
         throw error
       }
@@ -52,15 +63,202 @@ function CameraStream({videoWidth, videoHeight}) {
   }
 }, [videoComponent, videoWidth, videoHeight])
 
+useEffect(() => { // Called when canvasComponent is changed
+  if (canvasComponent.current != null) {
+    async function canvasLoader() {
+      try {
+
+      } catch (error) {
+        throw error
+      }
+
+  }
+  canvasLoader()
+}
+}, [videoComponent, videoWidth, videoHeight])
+
+  useEffect(() => {
+    async function posenetLoader() {
+      try {
+        let posenet_model = await posenet.load({
+          architecture: modelName,
+          outputStride: 16,
+          quantBytes: 2
+
+        });
+        updateModel(posenet_model);
+        console.log(`Current model is ${modelName}`);
+      } catch (error) {
+        console.log(error)
+        throw new Error('PoseNet failed to load');
+      } finally {
+        // setTimeout(() => {
+        //   this.setState({loading: false});
+        // }, 200);
+      }
+    }
+    posenetLoader(); // Loads PoseNet model whenever modelName changes
+  }, [modelName]);
+
+  const getPose = () => {
+    const getPoseFrame = async () => {
+      if (videoComponent != null &&
+          canvasComponent.current != null &&
+          model != null &&
+          ready) {
+
+        const canvasContext = canvasComponent.current.getContext('2d');
+        const {
+          minPoseConfidence,
+          minPartConfidence,
+          videoWidth,
+          videoHeight,
+          showVideo,
+          showPoints,
+          showSkeleton,
+          skeletonColor,
+          skeletonLineWidth
+        } = props;
+        const poses = [];
+        const pose = await model.estimateSinglePose(videoComponent.current, {
+          video: true,
+          flipHorizontal: true
+        });
+        poses.push(pose);
+
+        canvasContext.clearRect(0, 0, videoWidth, videoHeight);
+
+        if (showVideo) {
+          canvasContext.save();
+          canvasContext.scale(-1, 1);
+          canvasContext.translate(-videoWidth, 0);
+          canvasContext.drawImage(videoComponent.current, 0, 0, videoComponent.current.width, videoComponent.current.height);
+          canvasContext.restore();
+        }
+        poses.forEach(({score, keypoints}) => {
+          if (score >= minPoseConfidence) {
+            if (showPoints) {
+              drawKeyPoints(
+                keypoints,
+                minPartConfidence,
+                skeletonColor,
+                canvasContext
+              );
+            }
+            if (showSkeleton) {
+              drawSkeleton(
+                keypoints,
+                minPartConfidence,
+                skeletonColor,
+                skeletonLineWidth,
+                canvasContext
+              );
+            }
+          }
+        })
+      }
+    }
+
+    setInterval(getPoseFrame, 1000/30)
+  }
+
+  getPose();
   return (
     <Container fluid>
       <h1>Camera</h1>
       <video
         playsInline
         ref={videoComponent}
+        style={{display: 'none', width: '100%'}}
       />
+      <canvas ref={canvasComponent} style={{width: 1280, height: 720}}/>
     </Container>
   );
+}
+
+CameraStream.defaultProps = {
+    videoWidth: 1280,
+    videoHeight: 780,
+    flipHorizontal: true,
+    modelName: 'MobileNetV1',
+    algorithm: 'single-pose',
+    showVideo: true,
+    showSkeleton: true,
+    showPoints: true,
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+    maxPoseDetections: 2,
+    nmsRadius: 20,
+    outputStride: 32,
+    imageScaleFactor: 0.45,
+    skeletonColor: '#ffadea',
+    skeletonLineWidth: 6,
+    loadingText: 'Loading...please be patient...',
+  };
+
+function toTuple({x, y}) {
+  return [x, y]
+}
+
+function drawKeyPoints(
+    keypoints,
+    minConfidence,
+    skeletonColor,
+    canvasContext,
+    scale = 1,
+    pointRadius = 3
+  ) {
+    keypoints.forEach(keypoint => {
+      if (keypoint.score >= minConfidence) {
+        console.log("drawKeyPoints");
+        const {x, y} = keypoint.position
+        canvasContext.beginPath()
+        canvasContext.arc(x * scale, y * scale, pointRadius, 0, 2 * Math.PI)
+        canvasContext.fillStyle = skeletonColor
+        canvasContext.fill()
+      }
+    })
+}
+
+function drawSegment(
+  [firstX, firstY],
+  [nextX, nextY],
+  color,
+  lineWidth,
+  scale,
+  canvasContext
+  ) {
+    canvasContext.beginPath()
+    canvasContext.moveTo(firstX * scale, firstY * scale)
+    canvasContext.lineTo(nextX * scale, nextY * scale)
+    canvasContext.lineWidth = lineWidth
+    canvasContext.strokeStyle = color
+    canvasContext.stroke()
+}
+
+function drawSkeleton(
+  keypoints,
+  minConfidence,
+  color,
+  lineWidth,
+  canvasContext,
+  scale = 1
+) {
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints,
+    minConfidence
+  )
+
+  adjacentKeyPoints.forEach(keypoints => {
+    drawSegment(
+      toTuple(keypoints[0].position),
+      toTuple(keypoints[1].position),
+      color,
+      lineWidth,
+      scale,
+      canvasContext
+    )
+  })
 }
 
 export default CameraStream;
