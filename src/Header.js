63121e522e50b070f8fbe7b5b1d11f45d7a63ccb
Oliver <oliver.redeyoff@gmail.com>
2021-01-16 19:35:43 +0000

    Added workouts.json and design workout selector page

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+    Fade
+  } from "shards-react";
 
 function Header() {
     return(
         <div className="header">
-            <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-            {/* M 1 62 C 93 85 139 45 295 65 C 429 84 470 75 500 67 L 500 0 L 0 0 Z */}
+            {/* <svg viewBox="0 0 500 150" preserveAspectRatio="xMinYMin meet">
                 <path d="M 1 62 C 93 85 139 45 295 65 C 429 84 470 75 500 67 L 500 0 L 0 0 Z" style={{stroke: "none"}}></path>
-            </svg>
-
+            </svg> */}
+            <div className="background"></div>
             <h1>Asany</h1>
-
-            <div className="account">
-                <div className="user-image"></div>
-                <div className="username">Will Cruse</div>
-            </div>
         </div>
     );
 }
---
+++ src/pages/workoutSelector.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import {
+    Fade,
+    Card,
+    CardHeader,
+    CardTitle,
+    CardImg,
+    CardBody,
+    CardFooter,
+    Button,
+    Container,
+    Row,
+    Col
+  } from "shards-react";
+import workouts from "../workouts"
+
+class workoutSelector extends React.Component {
+
+    constructor() {
+        super()
+        this.state = []
+        this.useAddFadeIn = this.useAddFadeIn.bind(this)
+    }
+
+    componentDidMount() {
+        var tempFadeIns = workouts.map(() => false);
+        console.log(tempFadeIns)
+        this.setState(tempFadeIns);
+        setTimeout(this.useAddFadeIn, 700);
+    }
+
+    useAddFadeIn() {
+        console.log(this.state);
+        for (const [key, value] of Object.entries(this.state)) {
+            console.log(key);
+            if(!this.state[key]) {
+                var temp = this.state
+                temp[key] = true
+                this.setState(temp)
+                setTimeout(this.useAddFadeIn, 100);
+                break
+            }
+        }
+    }
+
+    render() {
+
+        const workoutCards = workouts.map((workout, index) => 
+            <Col sm="6" lg="4">
+                <Fade in={this.state[index]}>
+                    <Card style={{ maxWidth: "300px", marginRight: "20px", marginLeft: "20px", marginBottom: "40px" }}>
+                        <CardImg top src={workout.imageURL} />
+                        <CardBody>
+                            <CardTitle>{workout.name}</CardTitle>
+                            <p>{workout.description}</p>
+                            <Button>Start &rarr;</Button>
+                        </CardBody>
+                    </Card>
+                </Fade>
+            </Col>
+        )
+
+        return (
+            <Container>
+                <Row>
+                    {workoutCards}
+                </Row>
+            </Container>
+        )
+    }
+
+  }
+
+  export default workoutSelector;
+  
