78e10ca1c183caef129290d23a6d5be8c678f16c
Will Cruse <willcruse123@protonmail.com>
2021-01-17 12:36:10 +0000

    Make

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 import os
+from dataclasses import dataclass
 
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from opentok import OpenTok
+from opentok import OpenTok, Session
 
 load_dotenv()
 
@@ -17,30 +18,50 @@ opentok = OpenTok(VONAGE_API_KEY, VONAGE_API_SECRET)
 
 sessions = {}
 
+@dataclass
+class WorkoutSession:
+    session: Session
+    workout: int
+
 @app.route("/new-session")
 def new_session():
     try:
         session = opentok.create_session()
-        sessions[session.session_id] = session
-        print(sessions)
+        sessions[session.session_id] = WorkoutSession(session, -1)
         return jsonify({'sessionID': session.session_id})
-    except Exception as e:
-        print(e)
+    except Exception:
         return jsonify({'error': 'unknown-error'})
 
 @app.route("/get-token", methods=["POST"])
 def get_token():
-    try:
+    try
         request_json = request.get_json()
         if 'sessionID' not in request_json.keys():
             return jsonify({'error': 'sessionID key is missing'})
 
-        session = sessions.get(request_json['sessionID'])
-        print(sessions)
-        if session is None:
+        workout_session = sessions.get(request_json['sessionID'])
+        if workout_session is None:
             return jsonify({'error': 'invalid sessionID'})
 
-        return jsonify({'token': session.generate_token()})
-    except Exception as e:
-        print(e)
-        return jsonify({'error': 'unknown-error'})    
+        return jsonify({'token': workout_session.session.generate_token()})
+    except Exception:
+        return jsonify({'error': 'unknown-error'})
+
+@app.route("/set-workout", methods=["POST"])
+def set_workout():
+    try:
+        request_json = request.get_json()
+        if not all(req_key in request_json.keys() for req_key in ['sessionID', 'workout']):
+            return jsonify({'error': 'key is missing'})
+
+        workout_session = sessions.get(request_json['sessionID'])
+        if workout_session is None:
+            return jsonify({'error': 'invalid sessionID'})
+
+        workout_session.workout = request_json['workout']
+        return jsonify({'success': True})
+    except Exception:
+        return jsonify({'error': 'unknown-error'})
+
+if __name__=='__main__':
+    load_dotenv()
--- backend/requests_test.py
+++ backend/requests_test.py
@@ -7,3 +7,6 @@ req_json = req.json()
 print(req_json)
 req_token = requests.post("http://localhost:5000/get-token", json={'sessionID': req_json['sessionID']})
 print(req_token.text)
+
+req_token = requests.post("http://localhost:5000/set-workout", json={'sessionID': req_json['sessionID'], 'workout': 1})
+print(req_token.text)
