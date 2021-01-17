51e3e5b17760f7c8a050cebc7a99d00493623e5b
Will Cruse <willcruse123@protonmail.com>
2021-01-17 00:41:33 +0000

    Make a backend which works with the sample react app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from opentok import OpenTok
 
@@ -13,10 +13,25 @@ CORS(app)
 
 opentok = OpenTok(VONAGE_API_KEY, VONAGE_API_SECRET)
 
-@app.route("/")
-def hello_world(self):
-    return "Hello Worlds"
+sessions = {}
 
+@app.route("/new-session")
+def new_session():
+    session = opentok.create_session()
+    sessions[session.session_id] = session
+    return jsonify({'sessionID': session.session_id})
+
+@app.route("/get-token", methods=["POST"])
+def get_token():
+    request_json = request.get_json()
+    if 'sessionID' not in request_json.keys():
+        return jsonify({'error': 'sessionID key is missing'})
+
+    session = sessions.get(request_json['sessionID'])
+    if session is None:
+        return jsonify({'error': 'invalid sessionID'})
+
+    return jsonify({'token': session.generate_token()})
 
 if __name__=='__main__':
     load_dotenv()
